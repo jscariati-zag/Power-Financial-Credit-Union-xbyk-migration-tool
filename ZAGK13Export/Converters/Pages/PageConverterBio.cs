819f314064b904ecaa47308cbf31b190d215a9a5
pandrews <pandrews@zaginteractive.com>
2025-04-24 17:43:25 -0400

    Add bio page converter

---
+++ ZAGK13Export/Converters/Pages/PageConverterBio.cs
@@ -0,0 +1,76 @@
+﻿using CMS.DocumentEngine;
+using Common;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZAGK13Export.Services;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+namespace ZAGK13Export.Converters.Pages
+{
+    class PageConverterBio : IPageConverter
+    {
+        public string Type => "custom.PageBio";
+        public string TargetType => "Custom.WebPage_Bio";
+        private readonly IConfiguration _config;
+        private readonly FieldConverters _fieldConverters;
+        private readonly CommonConverterService _commonConverterService;
+
+        public PageConverterBio(IConfiguration config, FieldConverters fieldConverters, CommonConverterService commonConverterService)
+        {
+            _config = config;
+            _fieldConverters = fieldConverters;
+            _commonConverterService = commonConverterService;
+        }
+
+        public Page Convert(TreeNode page)
+        {
+            var newPage = new Page
+            {
+                OldGuid = page.NodeGUID,
+                Type = "Page",
+                DisplayName = page.DocumentName,
+                ContentType = TargetType,
+                WidgetConfiguration = _commonConverterService.ConvertPageWidgets(page, _config.GetValue<string>("ComponentContainerType"), "EditableArea_01"),
+                TemplateConfiguration = new TemplateConfiguration
+                {
+                    identifier = "Custom.WebPage.Bio"
+                },
+                Language = _config.GetValue<string>("TargetLanguage"),
+                UrlSlug = page.NodeAlias,
+                Order = page.NodeOrder,
+                Published = page.IsPublished,
+                ItemData = new Dictionary<string, object>
+                {
+                    { "WebPage_Content_Name", page.DocumentName },
+                    { "WebPage_Inclusions_Search", !page.DocumentSearchExcluded },
+                    { "WebPage_Inclusions_SitemapHtml", !page.GetBooleanValue("DocumentSitemapExcluded", false) },
+                    { "WebPage_Inclusions_SitemapXml", !page.GetBooleanValue("DocumentSitemapExcluded", false) },
+                    { "WebPage_Seo_MetaTitle", page.DocumentPageTitle },
+                    { "WebPage_Seo_MetaDescription", page.DocumentPageDescription },
+                    { "WebPage_Seo_MetaKeywords", page.DocumentPageKeyWords },
+                    { "WebPage_Seo_SchemaContent", page.GetValue("PageBaseSchemaContent", "") },
+                    { "WebPage_Seo_CanonicalUrl", page.GetValue("PageBaseCanonicalUrl", "") },
+                    { "WebPage_Og_Title", page.GetValue("PageBaseOpenGraphTitle", "") },
+                    { "WebPage_Og_Type", page.GetValue("PageBaseOpenGraphType", "") },
+                    { "WebPage_Og_Description", page.GetValue("PageBaseOpenGraphDescription", "") },
+                    { "WebPage_Og_Image", _fieldConverters.ConvertMediaItemReference(page.GetValue("PageBaseOpenGraphImage", "")) },
+                    { "WebPage_BioName", page.GetValue("BioName", "") },
+                    { "WebPage_BioTitle", page.GetValue("BioTitle", "") },
+                    { "WebPage_BioImage", _fieldConverters.ConvertMediaItemReference(page.GetValue("BioImage", "")) },
+                    { "WebPage_BioPhone", page.GetValue("BioPhone", "") },
+                    { "WebPage_BioEmailAddress", page.GetValue("BioEmailAddress", "") },
+                    { "WebPage_BioCtas", _fieldConverters.ConvertCtas(page.GetValue("BioCtas", "")) },
+                    { "WebPage_BioSpecialties", page.GetValue("BioSpecialties", "") },
+                    { "WebPage_BioAddress", page.GetValue("BioAddress", "") },
+                    { "WebPage_BioContent", page.GetValue("BioContent", "") }
+                }
+            };
+
+            return newPage;
+        }
+    }
+}
