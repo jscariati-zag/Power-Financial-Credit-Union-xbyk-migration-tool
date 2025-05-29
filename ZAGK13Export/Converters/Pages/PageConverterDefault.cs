796868710f4094b37a828394aa73b6dad539f166
pandrews <pandrews@zaginteractive.com>
2025-05-28 21:10:52 -0400

    Create special widget converter for BancFirst default page type to handle placement of widgets into editable areas based on Key field

--- ZAGK13Export/Converters/Pages/PageConverterDefault.cs
+++ ZAGK13Export/Converters/Pages/PageConverterDefault.cs
@@ -35,7 +35,7 @@ namespace ZAGK13Export.Converters.Pages
                 Type = "Page",
                 DisplayName = page.DocumentName,
                 ContentType = TargetType,
-                WidgetConfiguration = _commonConverterService.ConvertPageWidgets(page, _config.GetValue<string>("ComponentContainerType"), "EditableArea_01"),
+                WidgetConfiguration = _commonConverterService.ConvertDefaultPageWidgets(page, _config.GetValue<string>("ComponentContainerType")),
                 TemplateConfiguration = new TemplateConfiguration
                 {
                     identifier = "Custom.WebPage.Default"
--- ZAGK13Export/Services/CommonConverterService.cs
+++ ZAGK13Export/Services/CommonConverterService.cs
@@ -105,5 +105,176 @@ namespace ZAGK13Export.Services
 
             return widgetConfiguration;
         }
+
+        public WidgetConfiguration? ConvertDefaultPageWidgets(TreeNode page, string containerType)
+        {
+            WidgetConfiguration? widgetConfiguration = new WidgetConfiguration
+            {
+                editableAreas = new List<EditableArea>()
+            };
+
+            if (page.NodeHasChildren)
+            {
+                IEnumerable<TreeNode> containers = new MultiDocumentQuery()
+                                .Path(page.NodeAliasPath, PathTypeEnum.Children)
+                                .OnSite(_config.GetValue<string>("SourceSite"))
+                                .Types(containerType)
+                                .Culture(_config.GetValue<string>("Culture"))
+                                .WithCoupledColumns()
+                                .NestingLevel(1)
+                                .Published()
+                                .OrderBy(n => n.NodeOrder);
+
+                if (containers.Any())
+                {
+                    string sectionGuid = Guid.NewGuid().ToString();
+
+                    foreach (var container in containers)
+                    {
+                        if (container.NodeHasChildren)
+                        {
+                            var primaryEditableArea = new EditableArea
+                            {
+                                identifier = "EditableArea_Primary"
+                            };
+
+                            List<Section> sections = new List<Section>();
+                            Section currentSection = _defaultSectionConverter.Convert(null);
+                            sections.Add(currentSection);
+
+                            IEnumerable<TreeNode> components = new MultiDocumentQuery()
+                                    .Path(container.NodeAliasPath, PathTypeEnum.Children)
+                                    .OnSite(_config.GetValue<string>("SourceSite"))
+                                    .Types(_widgetConverters.Select(c => c.Key).Concat(_sectionConverters.Select(c => c.Key)).ToArray())
+                                    .Culture(_config.GetValue<string>("Culture"))
+                                    .WithCoupledColumns()
+                                    .NestingLevel(1)
+                                    .Published()
+                                    .Where(n => n.GetValue<string>("Key", "Primary") == "Primary")
+                                    .OrderBy(n => n.NodeOrder);
+
+                            if (components.Any())
+                            {
+                                foreach (var component in components)
+                                {
+                                    if (_widgetConverters.TryGetValue(component.ClassName, out var widgetConverter))
+                                    {
+                                        if (currentSection.type != _defaultSectionConverter.TargetType)
+                                        {
+                                            currentSection = _defaultSectionConverter.Convert(null);
+                                            sections.Add(currentSection);
+                                        }
+                                        currentSection.zones[0].widgets.Add(Task.FromResult(widgetConverter.Convert(component)).Result);
+                                    }
+                                    else if (_sectionConverters.TryGetValue(component.ClassName, out var sectionConverter))
+                                    {
+                                        currentSection = Task.FromResult(sectionConverter.Convert(component)).Result;
+                                        sections.Add(currentSection);
+                                    }
+                                }
+                            }
+
+                            primaryEditableArea.sections = sections;
+                            widgetConfiguration.editableAreas.Add(primaryEditableArea);
+
+                            var columnsEditableArea = new EditableArea
+                            {
+                                identifier = "EditableArea_Columns"
+                            };
+
+                            List<Section> columnsSections = new List<Section>();
+                            Section currentColumnsSection = _defaultSectionConverter.Convert(null);
+                            columnsSections.Add(currentColumnsSection);
+
+                            IEnumerable<TreeNode> columnsComponents = new MultiDocumentQuery()
+                                    .Path(container.NodeAliasPath, PathTypeEnum.Children)
+                                    .OnSite(_config.GetValue<string>("SourceSite"))
+                                    .Types(_widgetConverters.Select(c => c.Key).Concat(_sectionConverters.Select(c => c.Key)).ToArray())
+                                    .Culture(_config.GetValue<string>("Culture"))
+                                    .WithCoupledColumns()
+                                    .NestingLevel(1)
+                                    .Published()
+                                    .Where(n => n.GetValue<string>("Key", "Primary") == "Columns")
+                                    .OrderBy(n => n.NodeOrder);
+
+                            if (columnsComponents.Any())
+                            {
+                                foreach (var component in columnsComponents)
+                                {
+                                    if (_widgetConverters.TryGetValue(component.ClassName, out var widgetConverter))
+                                    {
+                                        if (currentColumnsSection.type != _defaultSectionConverter.TargetType)
+                                        {
+                                            currentColumnsSection = _defaultSectionConverter.Convert(null);
+                                            columnsSections.Add(currentColumnsSection);
+                                        }
+                                        currentColumnsSection.zones[0].widgets.Add(Task.FromResult(widgetConverter.Convert(component)).Result);
+                                    }
+                                    else if (_sectionConverters.TryGetValue(component.ClassName, out var sectionConverter))
+                                    {
+                                        currentColumnsSection = Task.FromResult(sectionConverter.Convert(component)).Result;
+                                        columnsSections.Add(currentColumnsSection);
+                                    }
+                                }
+                            }
+
+                            columnsEditableArea.sections = columnsSections;
+                            widgetConfiguration.editableAreas.Add(columnsEditableArea);
+
+                            var secondaryEditableArea = new EditableArea
+                            {
+                                identifier = "EditableArea_Secondary"
+                            };
+
+                            List<Section> secondarySections = new List<Section>();
+                            Section currentSecondarySection = _defaultSectionConverter.Convert(null);
+                            secondarySections.Add(currentSecondarySection);
+
+                            IEnumerable<TreeNode> secondaryComponents = new MultiDocumentQuery()
+                                    .Path(container.NodeAliasPath, PathTypeEnum.Children)
+                                    .OnSite(_config.GetValue<string>("SourceSite"))
+                                    .Types(_widgetConverters.Select(c => c.Key).Concat(_sectionConverters.Select(c => c.Key)).ToArray())
+                                    .Culture(_config.GetValue<string>("Culture"))
+                                    .WithCoupledColumns()
+                                    .NestingLevel(1)
+                                    .Published()
+                                    .Where(n => n.GetValue<string>("Key", "Primary") == "Secondary")
+                                    .OrderBy(n => n.NodeOrder);
+
+                            if (secondaryComponents.Any())
+                            {
+                                foreach (var component in secondaryComponents)
+                                {
+                                    if (_widgetConverters.TryGetValue(component.ClassName, out var widgetConverter))
+                                    {
+                                        if (currentSecondarySection.type != _defaultSectionConverter.TargetType)
+                                        {
+                                            currentSecondarySection = _defaultSectionConverter.Convert(null);
+                                            secondarySections.Add(currentSecondarySection);
+                                        }
+                                        currentSecondarySection.zones[0].widgets.Add(Task.FromResult(widgetConverter.Convert(component)).Result);
+                                    }
+                                    else if (_sectionConverters.TryGetValue(component.ClassName, out var sectionConverter))
+                                    {
+                                        currentSecondarySection = Task.FromResult(sectionConverter.Convert(component)).Result;
+                                        secondarySections.Add(currentSecondarySection);
+                                    }
+                                }
+                            }
+
+                            secondaryEditableArea.sections = secondarySections;
+                            widgetConfiguration.editableAreas.Add(secondaryEditableArea);
+                        }
+                    }
+                }
+            }
+
+            if (!widgetConfiguration.editableAreas.Any())
+            {
+                return null;
+            }
+
+            return widgetConfiguration;
+        }
     }
 }
